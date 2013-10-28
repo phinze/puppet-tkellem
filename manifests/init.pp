4de9b5c1aa80b84a6d5ce0fc1078736bee75c62f
Paul Hinze <paulh@instructure.com>
2013-10-28 13:52:48 -0500

    add tkellem::listen and stub for tkellem::network

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,14 @@
-class tkellem {
+class tkellem(
+  $default_listen = true
+){
   include tkellem::prereqs
   include tkellem::package
   include tkellem::service
+
+  if ($default_listen) {
+    tkellem::listen { 'default': }
+    Class['tkellem::service'] -> Tkellem::Listen['default']
+  }
 
   Class['tkellem::prereqs'] ->
     Class['tkellem::package'] ->
---
+++ manifests/listen.pp
@@ -0,0 +1,19 @@
+define tkellem::listen(
+  $port         = 8765,
+  $ssl          = true,
+  $bind_address = '0.0.0.0'
+){
+  $protocol = $ssl ? {
+    false => 'irc',
+    true  => 'ircs'
+  }
+  $address = "${protocol}://${bind_address}:${port}"
+
+  exec { "add-tkellem-listen-${title}":
+    user        => 'tkellem',
+    environment => 'HOME=/home/tkellem',
+    command     => "/usr/local/bin/tkellem admin 'listen ${address}'",
+    unless      => "/usr/local/bin/tkellem admin 'listen' | /bin/grep '${address}'",
+    require     => Class['tkellem::service']
+  }
+}
