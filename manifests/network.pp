b5d1ad21c4a138f95247f84f88542175782924b0
Zach Wily <zach@zwily.com>
2013-10-28 22:13:45 -0600

    flesh out the network class some more

--- manifests/network.pp
+++ manifests/network.pp
@@ -1,4 +1,31 @@
 define tkellem::network(
-
+  $networkname = $title,
+  $host,
+  $port = 6697,
+  $ssl = true,
+  $public = false
 ){
+  $protocol = $ssl ? {
+    false => 'irc',
+    true  => 'ircs'
+  }
+  $address = "${protocol}://${host}:${port}/"
+
+  $public_opt = $public ? {
+    false => '',
+    true  => '--public'
+  }
+
+  $tkellem_db = '/var/lib/tkellem/.tkellem/tkellem.sqlite3'
+  $network_exists_sql = "'select count(*) from networks where name = \"${networkname}\"'"
+
+  # TODO: interrogate existing config and see if the network needs updating (hosts added/removed, etc)
+
+  exec { "add-tkellem-network-${networkname}":
+    user        => 'tkellem',
+    environment => 'HOME=/var/lib/tkellem',
+    command     => "/usr/local/bin/tkellem admin 'network ${public_opt} --name=${networkname} ${address}'",
+    unless      => "/usr/bin/[ $(usr/bin/sqlite3 ${tkellem_db} ${network_exists_sql}) != \"0\" ]"
+  }
+
 }
