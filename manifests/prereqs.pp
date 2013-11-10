3e0ab4faac652acdf0e28569213dc554132d3d54
Zach Wily <zach@zwily.com>
2013-11-09 22:26:26 -0700

    ability to specify logdir for tkellem

--- manifests/prereqs.pp
+++ manifests/prereqs.pp
@@ -1,4 +1,6 @@
-class tkellem::prereqs {
+class tkellem::prereqs(
+  $logdir = "/var/lib/tkellem/.tkellem/logs"
+) {
   package { [
     'build-essential',
     'libsqlite3-dev',
@@ -12,5 +14,28 @@ class tkellem::prereqs {
     ensure     => present,
     home       => '/var/lib/tkellem',
     managehome => true
+  }
+
+  file { '/var/lib/tkellem/.tkellem':
+    ensure => directory,
+    owner  => 'tkellem',
+    group  => 'tkellem'
+  }
+
+  # If the logdir is different from the default, we need to
+  # create a symlink before the process starts. (Better would
+  # be to make tkellem configurable.)
+  if $logdir != '/var/lib/tkellem/.tkellem/logs' {
+    file { $logdir:
+      ensure => directory,
+      owner  => 'tkellem',
+      group  => 'tkellem',
+      mode   => 0755
+    }
+
+    file { '/var/lib/tkellem/.tkellem/logs':
+      ensure => link,
+      target => $logdir
+    }
   }
 }
